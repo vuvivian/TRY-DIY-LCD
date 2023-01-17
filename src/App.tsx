8b5aed3f03fa3c501470a5de110adc886c181763
wangjuan <wangjuan3@chanjet.com>
2023-01-17 10:35:08 +0800

    🚀 集成antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import type { FC } from 'react';
+import { ConfigProvider, Button } from 'antd';
+import zhCN from 'antd/locale/zh_CN';
+import 'antd/dist/reset.css';
 import './App.less'
 
-function App() {
-  return (
-    <div className="App">
-     APP
-    </div>
-  );
-}
+const App: FC = () => (
+  <div className="App">
+    <ConfigProvider
+     locale={zhCN}
+     theme={{
+      token: {
+        colorPrimary: '#00b96b',
+      },
+    }}
+    >
+      <Button type="primary">Button</Button>
+    </ConfigProvider>
+  </div>
+);
 
 export default App;
